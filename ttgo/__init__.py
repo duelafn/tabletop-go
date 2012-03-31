3e1d00abe60c2cf870bbe2fea74e5d1139c95c92
Dean Serenevy <dean@serenevy.net>
2012-03-31 14:16:28 -0400

    window resizing

--- ttgo/__init__.py
+++ ttgo/__init__.py
@@ -19,7 +19,7 @@ from kivy.factory import Factory
 from kivy.graphics import Rectangle, Line, Color, Ellipse
 from kivy.lang import Builder
 from kivy.logger import Logger
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty, BooleanProperty
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty, BooleanProperty, OptionProperty, AliasProperty
 from kivy.resources import resource_find
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -30,67 +30,90 @@ from kivy.uix.widget import Widget
 
 class NumChooser(BoxLayout):
     app = ObjectProperty(None)
+Factory.register("NumChooser", NumChooser)
 
 class GoStone(Widget):
     color = StringProperty(None)
     annotation = StringProperty("")
+Factory.register("GoStone", GoStone)
 
 class NewGame(AnchorLayout):
     app = ObjectProperty(None)
+Factory.register("NewGame", NewGame)
 
 
 
-class GoGame(AnchorLayout):
-    stones  = NumericProperty(None)
-    player  = StringProperty("black")
-    app     = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        super(GoGame, self).__init__(**kwargs)
-        s = min( Config.getint('graphics', 'width'), Config.getint('graphics', 'height') ) - 50
-        self.board = GoBoard( stones=self.stones, size=(s,s), game=self )
-        self.add_widget( self.board )
+class GoGame(BoxLayout):
+    app = ObjectProperty(None)
+    board = ObjectProperty(None)
+    points = NumericProperty(None)
+    current_player = OptionProperty("black", options=["black","white"])
 
     def on_play(self,i,j):
-        if self.player == 'black':
-            self.player = 'white'
+        if self.current_player == 'black':
+            self.current_player = 'white'
         else:
-            self.player = 'black'
+            self.current_player = 'black'
+
+Factory.register("GoGame", GoGame)
 
 
-class GoBoard(Scatter):
-    game    = ObjectProperty(None)
-    stones  = NumericProperty(0)
-    padding = NumericProperty(10)
-    turn    = NumericProperty(1)
+class GoBoard(Widget):
+    game       = ObjectProperty(None)
+    turn       = NumericProperty(1)
+
+    board_pad  = NumericProperty(10)
+    points     = NumericProperty(None)
+    box_size   = NumericProperty(None)
+    stone_size = NumericProperty(None)
 
     def __init__(self, **kwargs):
         super(GoBoard, self).__init__(**kwargs)
-        self.touch = {}
+        self.touch  = {}
+        self.stones = []
+        self.bind(points=self.build)
+        self.bind(size=self.rescale, board_pad=self.rescale)
 
-        self._boxwidth  = int( (self.width  - 2 * self.padding) / self.stones )
-        self._boxheight = int( (self.height - 2 * self.padding) / self.stones )
-        self.stone_size = min( self._boxwidth, self._boxheight )
-
-        top  = int( ( self.height - self.stones * self._boxheight ) / 2 )
-        left = int( ( self.width  - self.stones * self._boxwidth  ) / 2 )
-        bottom = self.height - self.stones * self._boxheight - top
-        right  = self.width  - self.stones * self._boxwidth  - left
-        self._padding = { 'top': top, 'right': right, 'bottom': bottom, 'left': left }
-        self._px = px = { 'top': self.height-top, 'right': self.width-right, 'bottom': bottom, 'left': left }
-
-        self.board = [ [ None for i in xrange(self.stones) ] for j in xrange(self.stones) ]
+    def build(self, obj, n):
+        self.canvas.clear()
         with self.canvas:
-            Rectangle(source="board.png", size=self.size)
+            self.image  = Rectangle( source="board.png", size=(100,100) )
             Color(0,0,0)
-            x_min = left + int( self._boxwidth / 2 )
-            x_max = x_min + self._boxwidth * (self.stones - 1)
-            y_min = bottom + int( self._boxheight / 2 )
-            y_max = y_min + self._boxheight * (self.stones - 1)
-            for x in xrange(x_min, x_max+1, self._boxwidth):
-                Line( points=( x,y_min, x,y_max ) )
-            for y in xrange(y_min, y_max+1, self._boxheight):
-                Line( points=( x_min,y, x_max,y ) )
+            self.hlines = [ Line(points=(0,0,1,1)) for i in xrange(self.points) ]
+            self.vlines = [ Line(points=(0,0,1,1)) for i in xrange(self.points) ]
+        self.board = [ [ None for i in xrange(self.points) ] for j in xrange(self.points) ]
+        self.rescale(self, self.size)
+
+    def rescale(self, obj, size):
+        s = min(*self.size)
+
+        # Board image
+        self.image.size = (s,s)
+        self.image.pos  = (x - int(s/2) for x in self.center)
+
+        # Calculations
+        self.grid_pos   = (self.image.pos[0] + self.board_pad, self.image.pos[1] + self.board_pad)
+        self.box_size   = int( ( s - 2 * self.board_pad ) / self.points )
+        self.stone_size = self.box_size - 2
+
+        # Stone images
+        for i in xrange(self.points):
+            for j in xrange(self.points):
+                if self.board[i][j]:
+                    self.board[i][j].size   = (self.stone_size, self.stone_size)
+                    self.board[i][j].center = self.address2xy(i, j)
+
+        # Lines
+        (x_min,y_min) = self.address2xy(0,0)
+        (x_max,y_max) = self.address2xy(self.points-1,self.points-1)
+
+        for i in xrange(self.points):
+            (x,y) = self.address2xy( i, i )
+            self.hlines[i].points = [x_min,y,x_max,y]
+            self.vlines[i].points = [x,y_min,x,y_max]
+
+        self.canvas.ask_update()
+
 
     def _clip_to(self, a, low, high):
         if a < low:
@@ -100,21 +123,17 @@ class GoBoard(Scatter):
         return a
 
     def find_address(self, x, y):
-        i = self._clip_to( int(( x - self._px['left'] )   / self._boxwidth),   0, self.stones - 1 )
-        j = self._clip_to( int(( y - self._px['bottom'] ) / self._boxheight),  0, self.stones - 1 )
+        i = self._clip_to( int(( x - self.grid_pos[0] ) / self.box_size), 0, self.points - 1 )
+        j = self._clip_to( int(( y - self.grid_pos[1] ) / self.box_size), 0, self.points - 1 )
         return (i,j)
 
     def address2xy(self, i, j):
-        x = int( self._px['left']   + i * self._boxwidth  + ( self._boxwidth  - self.stone_size ) / 2 )
-        y = int( self._px['bottom'] + j * self._boxheight + ( self._boxheight - self.stone_size ) / 2 )
+        x = int( self.grid_pos[0] + i * self.box_size + self.box_size / 2 )
+        y = int( self.grid_pos[1] + j * self.box_size + self.box_size / 2 )
         return (x,y)
 
     def on_touch_down(self,touch):
         if self.collide_point(*touch.pos):
-#            super(GoBoard, self).on_touch_down(touch)# while testing transforsm
-#            if touch.is_double_tap:
-#                # TODO: "select" group (alpha=.5), determine if it is captured, and capture (or show a Bubble)
-#                pass
 
             touch.grab(self)
             tpos = self.to_local(*touch.pos)
@@ -124,7 +143,7 @@ class GoBoard(Scatter):
                 self.touch[touch.uid] = self.board[i][j]
                 self.board[i][j] = None
             else:
-                self.touch[touch.uid] = GoStone(width=self.stone_size, height=self.stone_size, color=self.game.player)
+                self.touch[touch.uid] = GoStone(width=self.stone_size, height=self.stone_size, color=self.game.current_player)
                 self.touch[touch.uid].center = tpos
                 self.touch[touch.uid].annotation = str(self.turn)
                 self.turn = self.turn + 1
@@ -134,21 +153,21 @@ class GoBoard(Scatter):
 
     def on_touch_move(self,touch):
         if touch.grab_current is self and self.touch.get(touch.uid, None):
-#            super(GoBoard, self).on_touch_move(touch)# while testing transforsm
             self.touch[touch.uid].center = self.to_local(*touch.pos)
             return True
 
     def on_touch_up(self,touch):
         if touch.grab_current is self and self.touch.get(touch.uid, None):
-#            super(GoBoard, self).on_touch_up(touch)# while testing transforsm
-            # TODO: look for adjacent capture groups and either select or highlight (alpha=.5) group
             (i, j) = self.find_address(*self.to_local(*touch.pos))
             self.board[i][j] = self.touch[touch.uid]
             del self.touch[touch.uid]
-            ani = Animation( d=.1, t='in_out_sine', pos=self.address2xy(i,j) )
+            ani = Animation( d=.1, t='in_out_sine', center=self.address2xy(i,j) )
             ani.start( self.board[i][j] )
             self.game.on_play(i, j)
             return True
+
+Factory.register("GoBoard", GoBoard)
+
 
 class TTGoApp(App):
     title = "TTGo"
@@ -158,7 +177,6 @@ class TTGoApp(App):
         kivy.resources.resource_add_path("themes/" + self.config.get('ttgo', 'theme'))
         for kv in glob.glob('ttgo/*.kv'):
             if kv != 'ttgo/ttgo.kv': Builder.load_file(kv, rulesonly=True)
-        self.screens = { "new": NewGame(app=self) }
         self.goto_screen("new")
 
     def on_start(self):
@@ -181,14 +199,16 @@ class TTGoApp(App):
             size = int(size)
         except ValueError, e:
             return
-        self.screens["game"] = GoGame(app=self, stones=size)
-        self.goto_screen("game")
+        self.goto_screen("game", size=size)
 
-    def goto_screen(self, screen_name):
+    def goto_screen(self, screen_name, **opt):
         self.root.clear_widgets()
+        if   screen_name == "new":
+            widget = NewGame( app=self, size=self.root.size )
+        elif screen_name == "game":
+            widget = GoGame(app=self, points=opt['size'], size=self.root.size)
+        self.root.add_widget( widget )
         self.screen = screen_name
-        self.root.add_widget(self.screens[screen_name])
-
 
     def build_config(self, config):
         config.setdefaults('ttgo', {
@@ -217,8 +237,6 @@ class TTGoApp(App):
 ]
 """
         settings.add_json_panel('TTGo application', self.config, data=jsondata)
-
-Factory.register("NumChooser", NumChooser)
 
 if __name__ == '__main__':
     TTGoApp().run()
